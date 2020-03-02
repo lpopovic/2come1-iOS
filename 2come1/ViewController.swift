2acc78db627d12a50ab42987ae56d0e338a36991
Lazar Popovic <lazar.popovic.riitech.com>
2020-03-02 13:35:01 +0100

    Add Xcode project

---
+++ 2come1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2come1
+//
+//  Created by MacBook on 3/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
