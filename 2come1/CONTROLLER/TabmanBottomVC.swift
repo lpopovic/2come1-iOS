8dfb7e070b6df0c6ad1da816de6fd90adc710c4e
Lazar Popovic <lazar.popovic.riitech.com>
2020-03-03 15:19:25 +0100

    import localization in TabmanBottomVC

--- 2come1/CONTROLLER/TabmanBottomVC.swift
+++ 2come1/CONTROLLER/TabmanBottomVC.swift
@@ -11,41 +11,9 @@ import UIKit
 import Tabman
 import Pageboy
 
-extension UIView {
-    func addTopBorderWithColor(color: UIColor, width: CGFloat) {
-        let border = CALayer()
-        border.backgroundColor = color.cgColor
-        border.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: width)
-        self.layer.addSublayer(border)
-    }
-    
-    func addRightBorderWithColor(color: UIColor, width: CGFloat) {
-        let border = CALayer()
-        border.backgroundColor = color.cgColor
-        border.frame = CGRect(x: self.frame.size.width - width, y: 0, width: width, height: self.frame.size.height)
-        self.layer.addSublayer(border)
-    }
-    
-    func addBottomBorderWithColor(color: UIColor, width: CGFloat) {
-        let border = CALayer()
-        border.backgroundColor = color.cgColor
-        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: width)
-        self.layer.addSublayer(border)
-    }
-    
-    func addLeftBorderWithColor(color: UIColor, width: CGFloat) {
-        let border = CALayer()
-        border.backgroundColor = color.cgColor
-        border.frame = CGRect(x: 0, y: 0, width: width, height: self.frame.size.height)
-        self.layer.addSublayer(border)
-    }
-}
-
-
 class TabmanBottomVC: TabmanViewController {
     var nizVC :[UIViewController] = []
     let titleBarItems :[String] = ["Around", "Chat", "Match", "Profile"]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupArrayVC()
@@ -104,7 +72,7 @@ extension TabmanBottomVC: PageboyViewControllerDataSource {
 
 extension TabmanBottomVC: TMBarDataSource {
     func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
-        let title = self.titleBarItems[index]
+        let title = NSLocalizedString(self.titleBarItems[index],comment: "comment for user")
         
         switch index {
         case 0:
